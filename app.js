eab738cdae6f9ec5b2ba87951d9ac349d26aa0b3
Jacob Rigs <jacoberiggs@gmail.com>
2023-11-20 16:01:50 -0600

    server-update

--- app.js
+++ app.js
@@ -7,9 +7,55 @@ const groupController = require("./controllers/group.controller");
 const userController = require("./controllers/user.controller");
 const friendController = require("./controllers/friend.controller");
 
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const PORT = process.env.PORT;
 const DB_NAME = process.env.DB_NAME;
 const DB_URL = process.env.DB_URL;
+
+
+// const express = require("express");
+// const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+io.on("connection", (socket) => {
+  socket.broadcast.emit("hi");
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
 
 // ! Connect To Cluster Database
 mongoose.connect(DB_URL);
